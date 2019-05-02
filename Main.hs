40a88db5e433a99be4a776f70b131ee231df9d8b
Paul Kinsky <pkinsky@gmail.com>
2019-05-02 00:49:15 -0700

    holy crap, it works

--- Main.hs
+++ Main.hs
@@ -6,6 +6,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE GADTs #-}
 
 -- | Haskell module declaration
 module Main where
@@ -15,6 +16,9 @@ import Data.Aeson
 import Data.Monoid
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
+
+import qualified Data.ByteString.Base64 as Base64
+
 
 -- | Miso framework import
 import Miso
@@ -26,7 +30,12 @@ import Merkle.Higher.BitTorrent
 
 import JavaScript.Web.XMLHttpRequest
 
+import JavaScript.TypedArray.ArrayBuffer
+
 import Control.Concurrent.MVar
+
+
+import GHCJS.Buffer -- weird magic
 
 import GHCJS.Types
 import GHCJS.Foreign.Callback
@@ -34,25 +43,40 @@ import GHCJS.Foreign.Callback
 import Language.Javascript.JSaddle.Value
 import Language.Javascript.JSaddle.Marshal.String
 
+
+import Data.ByteString as BS (ByteString, pack, useAsCStringLen)
+
 import Data.ByteString.Lazy (toStrict)
 import Data.Singletons
 import Util.HRecursionSchemes
 
+import Foreign.Ptr (Ptr)
+
+import GHCJS.Marshal.Pure (pFromJSVal, pToJSVal)
+
+import GHCJS.DOM.Types (noBlobPropertyBag)
+
+import GHCJS.DOM.Blob (newBlob, Blob(..))
+
+
+
 type TorrentHash = Hash 'TorrentTag
+type TorrentLayer = BitTorrent Hash 'TorrentTag
+
+
 
 -- | Type synonym for an application model
 data Model
   = NoFocus -- starting state
-  | TorrentFocus TorrentHash -- after entering a hash
-  deriving (Eq, Show)
+  | TorrentFocus TorrentLayer -- after entering a hash
+  deriving (Eq)
 
 data Action
   = SelectTorrent TorrentHash
+  | FocusTorrent TorrentLayer -- once torrent dl done
   | DownloadFile FilePath
   | NoOp
-  | TestPut
-  | TestGet
-  deriving (Show, Eq)
+  deriving (Eq)
 
 -- | Entry point for a miso application
 main :: IO ()
@@ -62,39 +86,46 @@ main = startApp App {..}
     model         = NoFocus
     update        = updateModel
     view          = viewModel
-    events        = defaultEvents        -- default delegated events
-    subs          = []                   -- empty subscription list
-    mountPoint    = Nothing          -- mount point for application (Nothing defaults to 'body')
+    events        = defaultEvents -- default delegated events
+    subs          = []            -- empty subscription list
+    mountPoint    = Nothing       -- mount point for application (Nothing defaults to 'body')
 
 -- | Updates model, optionally introduces side effects
 updateModel :: Action -> Model -> Effect Action Model
-updateModel (SelectTorrent h) m = noEff m
-updateModel (DownloadFile fp) m = do
-  -- deref chunk
-  pure m
+updateModel (SelectTorrent h) m = m <# do
+
+  -- let t = exampleTorrent1
+  -- r <- cataM ipfsPut t >>= toJSVal . unHash
+  -- consoleLog r
+
+  t <- ipfsGet h
+  pure $ FocusTorrent t -- todo: should handle failure/not found case gracefully, eventually
+updateModel (FocusTorrent t) m = noEff $ TorrentFocus t
+updateModel (DownloadFile fp) NoFocus = noEff NoFocus -- just no-op? should never happen..
+updateModel (DownloadFile fp) m@(TorrentFocus (Torrent _ files)) = m <# do
+  Chunk bytes <- case (lookup fp files) of
+    Nothing -> error "file not found, fail - should never happen.."
+    Just h -> ipfsGet h
+
+  -- hax, convert back to b64 as string to get bytrestring across js ffi gap
+  let bytes' = T.unpack $ T.decodeLatin1 (Base64.encode bytes)
+
+  downloadFile (toJSString fp) (toJSString bytes')
+  pure NoOp
 updateModel NoOp m = noEff m
-updateModel TestPut m = m <# do
-  let t = exampleTorrent1
-  r <- cataM ipfsPut t >>= toJSVal . unHash
-  consoleLog r
-
-  pure NoOp
-updateModel TestGet m = m <# do
-  -- downloadFile "fname" "dGVzdA==" -- 'test' in base64
-  let h = Hash "QmVUM6tbpnoyto1CAktVAzsNM7JjU9G6dinMMGW1BM45Ke" :: Hash 'TorrentTag -- from put test
-  r <- ipfsGet h >>= toJSVal . toJSON
-  consoleLog r
-
-  downloadFile "fname" "dGVzdA==" -- 'test' in base64
-
-  pure NoOp
 
 -- | Constructs a virtual DOM from a model
 viewModel :: Model -> View Action
-viewModel x = div_ []
- [ button_ [ onClick TestPut ] [ text "test-put" ]
- , button_ [ onClick TestGet ] [ text "test-get" ]
+viewModel NoFocus = div_ []
+-- click to run on hardcoded torrent
+ [ button_ [ onClick $ SelectTorrent $ Hash "QmZ9BwD86pWrg8MNqWjRc85zyTWzN7vaxQpLDV8X8EoJWk"]
+           [ text "pick precoded torrent hash" ]
  ]
+viewModel (TorrentFocus (Torrent m fs)) = div_ [] $ [text $ toJSString m] ++ fs'
+  where
+    fs' = fmap mkF fs
+    mkF (fp, c) = button_ [ onClick $ DownloadFile fp]
+                          [ text $ toJSString fp]
 
 -- this works for test file/test b64!
 foreign import javascript unsafe
